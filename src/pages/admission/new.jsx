1ddc800acbd9d5eef2a7b423461c7fb177a769e5
northenlights <akash221002@gmail.com>
2024-02-26 01:07:18 +0530

    added warning for unique email in enquiry

--- src/pages/admission/new.jsx
+++ src/pages/admission/new.jsx
@@ -215,6 +215,9 @@ const NewAdmission = () => {
                     autoComplete="address-level2"
                     className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
+                  <p className="text-red-400 font-bol">
+                    *Email should be unique
+                  </p>
                 </div>
               </div>
               <div className="sm:col-span-2 sm:col-start-1 md:col-span-4">
